4e22f8c4f8d205045b33933c1562e21ac6aae29c
Pham Chu Duong <pduong244@gmail.com>
2023-06-20 23:27:04 +0700

    Login and Register logic

--- app/src/main/java/com/example/prm392/MainActivity.java
+++ app/src/main/java/com/example/prm392/MainActivity.java
@@ -1,32 +1,45 @@
 package com.example.prm392;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        ArrayList<Food> foods = new ArrayList<>();
-        Food food1 = new Food(R.drawable.ic_launcher_foreground, "Pork skin", "High fat");
-        Food food2 = new Food(R.drawable.ic_launcher_foreground, "Chicken breast", "High protein but low on fat");
-        foods.add(food1);
-        foods.add(food2);
-        FoodAdapter adapter = new FoodAdapter(foods);
-        RecyclerView rec = findViewById(R.id.rv_foods);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        rec.setLayoutManager(layoutManager);
-        rec.setAdapter(adapter);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user == null) {
+            GoToLogin();
+        } else {
+            setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+            ArrayList<Food> foods = new ArrayList<>();
+            Food food1 = new Food(R.drawable.ic_launcher_foreground, "Pork skin", "High fat");
+            Food food2 = new Food(R.drawable.ic_launcher_foreground, "Chicken breast", "High protein but low on fat");
+            foods.add(food1);
+            foods.add(food2);
+            FoodAdapter adapter = new FoodAdapter(foods);
+            RecyclerView rec = findViewById(R.id.rv_foods);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+            rec.setLayoutManager(layoutManager);
+            rec.setAdapter(adapter);
 
 //        ((Button) findViewById(R.id.btn_show_all)).setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -35,11 +48,27 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(intent);
 //            }
 //        });
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_logout) {
+            FirebaseAuth.getInstance().signOut();
+            GoToLogin();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void GoToLogin() {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/prm392/Register.java
+++ app/src/main/java/com/example/prm392/Register.java
@@ -1,14 +1,94 @@
 package com.example.prm392;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+    FirebaseAuth auth;
+    TextInputEditText edt_email, edt_password;
+    ProgressBar progressBar;
+    Button btn_register;
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            GoToLogin();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        auth = FirebaseAuth.getInstance();
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        progressBar = findViewById(R.id.progressBar);
+        btn_register = findViewById(R.id.btn_register);
+        textView = findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToLogin();
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = edt_email.getText().toString().trim(), password = edt_password.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    SignUp(email, password);
+                }
+            }
+        });
+    }
+
+    public void SignUp(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
+                            GoToLogin();
+                        } else {
+                            Toast.makeText(Register.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void GoToLogin() {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
     }
 }
